4588980b109bb09d2cbdbe32a9e2fe1dc47a4e83
Bharat Hegde <bharathegde@Bharats-MacBook-Air.local>
2025-05-11 11:51:20 +0530

    make config updaatable without restarting application

--- src/main/java/com/eazybytes/cards/dto/CardsContactInformation.java
+++ src/main/java/com/eazybytes/cards/dto/CardsContactInformation.java
@@ -1,14 +1,17 @@
 package com.eazybytes.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactInformation(
-        String message,
-        Map<String, String> contactDetails,
-        List<String> onCallSupport
-) {
+@Getter
+@Setter
+public class CardsContactInformation{
+    String message;
+    Map<String, String> contactDetails;
+    List<String> onCallSupport;
 }
